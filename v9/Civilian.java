c3b45f927191d6290bcadb8bb79edfbcec1d5d7c
xinrachel30 <xlin30@stuy.edu>
2022-01-23 21:24:51 -0500

    finishing touches

---
+++ v9/Civilian.java
@@ -0,0 +1,38 @@
+/*
+kleex -- Lindsay Phung and Xinqing Lin
+APCS pd07
+FP: Show Us What You Got
+2022-01-21
+*/
+
+public class Civilian extends Character {
+
+  protected String name;
+  protected boolean isAlive;
+
+  public Civilian() {
+    currency = 100;
+    knowledge = 10; 
+    isAlive = true;
+  }
+
+  public Civilian(String input) {
+    name = input;
+    currency = 100;
+    knowledge = 10;
+    isAlive = true;
+  }
+
+  public void setName(String input) {
+    name = input;
+  }
+
+  public void die() {
+    isAlive = false;
+  }
+
+  public String toString() {
+    return name;
+  }
+
+}
---
+++ v9/PrivateInvestigator.java
@@ -0,0 +1,109 @@
+/*
+kleex -- Lindsay Phung and Xinqing Lin
+APCS pd07
+FP: Show Us What You Got
+2022-01-21
+*/
+ 
+public class PrivateInvestigator extends Character {
+
+  protected String name;
+  protected int currency;
+  protected int relationKenna = 0;
+  protected int relationJimmy = 0;
+  protected boolean clueCafe = false;
+  protected boolean clueCoffee = false;
+  protected boolean clueMansion = false;
+  protected boolean clueBeach = false;
+  protected boolean clueKennaRant = false;
+  protected boolean clueAbsentParents = false;
+  protected boolean clueWrongKenna = false;
+  protected boolean clueJimmyButler = false;
+
+  public PrivateInvestigator(String input) {
+    currency = 10;
+    name = input;
+  }
+
+//Updt Values ----------------------------------------------------------------//
+
+  public void regularEnergy() {
+    energy --;
+  }
+
+  public void lotEnergy() {
+    energy -= 2;
+  }
+
+  public void upKnowledge() {
+    knowledge ++;
+  }
+
+  public void upEnergy() {
+    energy += 5;
+  }
+
+  public void dayEnergy() {
+    energy += 10;
+  }
+
+  public void payForGame() {
+    currency -= 10;
+  }
+
+  public void winGame() {
+    currency += 50;
+  }
+
+  public void draw(){
+    currency += 30;
+  }
+
+  public void Kenna() {
+    relationKenna ++;
+  }
+
+  public void Jimmy() {
+    relationJimmy ++;
+  }
+
+//Get Values -----------------------------------------------------------------//
+
+  public boolean checkMoney() {
+    if (currency >= 10) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public int getEnergy() {
+    return energy;
+  }
+
+  public int getKnowledge() {
+    return knowledge;
+  }
+
+  public int getMoney() {
+    return currency;
+  }
+
+  public void amountEnergy() {
+    System.out.println("You have " + energy + " energy.");
+  }
+
+  public void amountKnowledge() {
+    System.out.println("You have" + knowledge + "knowledge.");
+  }
+
+  public int getJimmy() {
+    return relationJimmy;
+  }
+
+  public int getKenna() {
+    return relationKenna;
+  }
+
+}
